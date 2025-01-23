e4e9b8a3390cd2ed55144971ce926f383215b42e
Abdul Wahab <abdulwahabee5@gmail.com>
2025-01-23 10:13:27 +0500

    updated code - used mongodb atlas instead of mongodb local

--- db.js
+++ db.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotebook";
+const mongoURI = "mongodb+srv://abdulwahabeeee5:myatlaspassword@cluster0.mujzo.mongodb.net/inotebook_db?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectToMongo = async ()=>{
     await mongoose.connect(mongoURI);
     console.log('MongoDB connected successfully');
 }
-
+// Cluster name: Cluster0
+// Username: abdulwahabeeee5
+// Password: myatlaspassword
+// Daatabase name: inotebook_db
 module.exports = connectToMongo;
